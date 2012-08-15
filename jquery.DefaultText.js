b0be3909b35b00c3b7b2ee2ee43b612eeee7f6ea
Matt Grundy <matt@mattgrundy.co.uk>
2012-08-15 14:49:18 +0100

    Added some closure

--- jquery.DefaultText.js
+++ jquery.DefaultText.js
@@ -1,50 +1,54 @@
-$.fn.DefaultText = function(options) {
+(function( $ ) {
 
-    var defaults = {
-        inactiveClass:      '',
-        inactiveFontStyle:  'italic',
-        inactiveColour:     'rgb(204,204,204)',
-        activeFontStyle:    'normal',
-        activeColour:       'rgb(0,0,0)'
-    };
+	$.fn.DefaultText = function(options) {
 
-    var opts = $.extend(defaults, options);
+	    var defaults = {
+	        inactiveClass:      '',
+	        inactiveFontStyle:  'italic',
+	        inactiveColour:     'rgb(204,204,204)',
+	        activeFontStyle:    'normal',
+	        activeColour:       'rgb(0,0,0)'
+	    };
+
+	    var opts = $.extend(defaults, options);
     
-    $(this).each(function(){
+	    $(this).each(function(){
         
-        if(opts.inactiveClass != ''){
-            $(this).addClass(opts.inactiveClass);
-        }else{
-            $(this).css({'font-style' : opts.inactiveFontStyle, 'color' : opts.inactiveColour});
-        }
+	        if(opts.inactiveClass != ''){
+	            $(this).addClass(opts.inactiveClass);
+	        }else{
+	            $(this).css({'font-style' : opts.inactiveFontStyle, 'color' : opts.inactiveColour});
+	        }
         
-        $(this).bind('focus', function(){
-            if($(this).val() == $(this).attr('title')){
-                if(opts.inactiveClass != ''){
-                    $(this).removeClass(opts.inactiveClass);
-                }else{
-                    $(this).css({'font-style' : opts.activeFontStyle, 'color' : opts.activeColour});
-                }
+	        $(this).bind('focus', function(){
+	            if($(this).val() == $(this).attr('title')){
+	                if(opts.inactiveClass != ''){
+	                    $(this).removeClass(opts.inactiveClass);
+	                }else{
+	                    $(this).css({'font-style' : opts.activeFontStyle, 'color' : opts.activeColour});
+	                }
                 
-                $(this).val('');
-            }
-        });
+	                $(this).val('');
+	            }
+	        });
         
-        $(this).bind('blur', function(){
-            if($(this).val() == ''){
-                if(opts.inactiveClass != ''){
-                    $(this).addClass(opts.inactiveClass);
-                }else{
-                    $(this).css({'font-style' : opts.inactiveFontStyle, 'color' : opts.inactiveColour});
-                }
+	        $(this).bind('blur', function(){
+	            if($(this).val() == ''){
+	                if(opts.inactiveClass != ''){
+	                    $(this).addClass(opts.inactiveClass);
+	                }else{
+	                    $(this).css({'font-style' : opts.inactiveFontStyle, 'color' : opts.inactiveColour});
+	                }
                 
-                $(this).val($(this).attr('title'));
-            }
-        });
+	                $(this).val($(this).attr('title'));
+	            }
+	        });
         
-        $(this).blur();
-        $(this).val($(this).attr('title'));
+	        $(this).blur();
+	        $(this).val($(this).attr('title'));
         
-    });
+	    });
     
-};
+	};
+
+})( jQuery );
